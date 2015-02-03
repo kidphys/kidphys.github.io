8c9871d56650280a6b14224bfa7a115da1de3b29
Chau Hoang <kidphys@gmail.com>
2015-02-03 13:37:53 +0700

    Polish UI - add loan source/add trade

---
+++ margin/margin.js
@@ -0,0 +1,26 @@
+var marginApp = angular.module('MarginApp', []);
+
+marginApp.controller('MarginAppController', function ($scope) {
+    $scope.account = {};
+    $scope.account.stockBook = [
+        {'symbol': 'VND', 'amount': 1000000},
+        {'symbol': 'SSI', 'amount': 2000000},
+    ];
+
+    $scope.loanCatalog = [
+        {
+            'name': 'mr',
+            'loans': [
+                {'symbol': 'VND', 'rate': 0.5},
+                {'symbol': 'SSI', 'rate': 0.6},
+            ]
+        },
+        {
+            'name': 'df',
+            'loans': [
+                {'symbol': 'ACB', 'rate': 0.3},
+                {'symbol': 'SSI', 'rate': 0.5},
+            ]
+        },
+    ]
+});
